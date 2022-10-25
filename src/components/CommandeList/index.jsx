283064c161d7047ab9efef02157ec865d67a038d
Nehemie Shangwe <shangwe.nehms@gmail.com>
2022-10-25 18:21:42 +0200

    intialized bon de commande

--- src/components/CommandeList/index.jsx
+++ src/components/CommandeList/index.jsx
@@ -1,66 +1,32 @@
-import React, { forwardRef, useState } from 'react';
-import { ActionIcon, Button, Group, Select, Text, TextInput } from '@mantine/core';
+import React, { useState } from 'react';
+import { ActionIcon, Button, Group, Text, TextInput } from '@mantine/core';
 import { BsBackspace, BsPlus, BsQuestionCircle } from 'react-icons/bs';
+import uuid from 'react-uuid'
 import { FaRegSave } from 'react-icons/fa';
 import { useForm } from '@mantine/form';
-import { useSelector } from 'react-redux';
 import { useModals } from '@mantine/modals';
 
 
-const SelectItem = forwardRef(
-    ({ label, pv_unit, value, ...others }, ref) => (
-        <div ref={ref} {...others}>
-            <Group noWrap>
-                <div style={{width: '100%', display: 'inline-flex', alignItems:'center', justifyContent: 'space-between'}}>
-                    <Text size='sm'>{label}</Text>
-                    <h3 style={{color:'dodgerblue', fontSize:16, fontWeight: '400'}}>$ {pv_unit?.toFixed(2)}</h3>
-                </div>
-            </Group>
-        </div>
-    )
-);
-
-function AchatList({ category, handleValidate }) {
+function CommandeList({ handleValidate }) {
     const modals = useModals()
     const [loading, setloading] = useState(false);
-    const stocks = useSelector(state => state.stocks);
     const [list, setList] = useState([])
-
-    const [selectedProduct, setSelectedProduct] = useState()
     const [sum, setSum] = useState(0)
 
-    
     const form = useForm({
         initialValues: {
             product: '',
             qty: 1,
-            pv_unit: parseInt(0).toFixed(2),
-            pv_tot: parseInt(0).toFixed(2)
+            pv_unit: parseInt(1).toFixed(2),
+            pv_tot: parseInt(1).toFixed(2)
         },
         validate: {
-            product: (value) => (value === null ? 'produit exigé' : null),
+            designation: (value) => (value === null ? 'designation exigé' : null),
             qty: (value) => parseInt(value) > 0 ? null : 'qté invalid',
             pv_unit: (value) => (value <= 0 ? 'pv invalid' : null),
         },
         validateInputOnChange: ['product', 'qty']
     })
-
-    const productProvider = !stocks ? [] : stocks?.map(product => {
-        return {
-          label: product?.designation,
-          pv_unit: product?.pv_gen,
-          value: product?._id
-        }
-    })
-
-    const handleChangeProduct = () => {
-        let product_id = form.values?.product
-        const selected = stocks.find(product => product._id === product_id)
-
-        setSelectedProduct(selected)
-        form.setFieldValue('pv_unit', selected?.pv_gen?.toFixed(2))
-        form.setFieldValue('pv_tot', (parseFloat(selected?.pv_gen) * parseFloat(form.values.qty))?.toFixed(2))
-    }
 
     const handleChangeNumber = () => {
         const pv_unit = form.values.pv_unit
@@ -69,28 +35,26 @@ function AchatList({ category, handleValidate }) {
         const pv_tot = parseFloat(pv_unit) * parseFloat(qty)
         form.setFieldValue('pv_tot', pv_tot.toFixed(2))
 
-        if(pv_unit < selectedProduct.pv_min) {
-            form.setFieldError('pv_unit', `must be >= ${selectedProduct.pv_min}`)
+        if(pv_unit <= 0 ) {
+            form.setFieldError('pv_unit', `must be > 0`)
         }
 
-        if(qty > selectedProduct.qty && category === 'facture') {
-            form.setFieldError('qty', `must be <= ${selectedProduct.qty}`)
+        if(qty < 1) {
+            form.setFieldError('qty', `must be >= 1`)
         }
     }
 
     const handleAddProduct = (e) => {
-        if(form.values.product === '' || form.values.pv_unit === 0) return 
+        if(form?.values?.designation?.length === 0 || form?.values?.pv_unit === 0) return 
 
-        if(list.find(o => o.product === form.values.product)) {
+        if(list.find(o => o.designation === form.values.designation)) {
             form.setFieldError('product', 'ce produit a déjà été selectionné')
             return
         }
 
         const newItem = {
-            product: form.values?.product,
-            category: selectedProduct?.category,
-            unit: selectedProduct?.unit,
-            designation: selectedProduct?.designation,
+            _id: uuid(),
+            designation: form.values?.designation,
             qty: parseFloat(form.values?.qty),
             pv_unit: parseFloat(form.values?.pv_unit),
             pv_tot: parseFloat(form.values.qty) * parseFloat(form.values.pv_unit)
@@ -106,17 +70,18 @@ function AchatList({ category, handleValidate }) {
     const handleRemoveProduct = (e, data) => {
        
         let newList = list.filter(function( obj ) {
-            return obj.product !== data.product;
+            return obj._id !== data._id;
           });
         setList(newList)
 
         const summation = newList.reduce((a, b) => a + (b['pv_tot'] || 0), 0)
         
         setSum(summation)
+        form.reset()
     }
 
     const rows = list?.map(data => (
-        <Group key={data.product} noWrap style={{borderBlock:'0.5px solid #eaeaea', marginBlock:2}}>
+        <Group key={data._id} noWrap style={{borderBlock:'0.5px solid #eaeaea', marginBlock:2}}>
             <h3 style={{width: '55%', padding: '4px 12px', borderInline:'1px solid #eaeaea', fontWeight:400, fontSize:14}}>{data?.designation}</h3>
             <h3 style={{width: '15%', padding: '4px 14px', borderInline:'1px solid #eaeaea', textAlign:'left', fontSize:14, fontWeight:400}}>{data?.qty} {data?.unit}</h3>
             <h3 style={{width: '15%', padding: '4px 14px',  borderInline:'1px solid #eaeaea', textAlign:'left', fontWeight:400}}>${(data?.pv_unit)?.toFixed(2)}</h3>
@@ -130,7 +95,7 @@ function AchatList({ category, handleValidate }) {
             title: <Text size='md' weight={700} style={{display:'inline-flex', alignItems:'center'}}><BsQuestionCircle size={18} style={{marginRight: 8}} />Valider ?</Text>,
             children: (
                 <Text size='xs'>
-                    Etes-vous sure de valider cet achat ?
+                    Etes-vous sure de valider cette commande ?
                 </Text>
             ),
             zIndex:201,
@@ -153,16 +118,13 @@ function AchatList({ category, handleValidate }) {
         <div  style={{borderTop: '1px solid #eaeaea', marginTop: -8}}>
             <form onSubmit={form.onSubmit(handleAddProduct)}>
                 <div className='dates' style={{justifyContent: 'space-between'}}>
-                    <Select 
-                        itemComponent={SelectItem}
-                        data={productProvider}
+                    <TextInput 
                         variant='filled' 
                         size='sm' 
                         style={{width:'48%'}} 
                         label="Produit :" 
                         required
-                        onSelect={handleChangeProduct}
-                        {...form.getInputProps('product')}
+                        {...form.getInputProps('designation')}
                         searchable
                         clearable
                         className='textinput' 
@@ -228,4 +190,4 @@ function AchatList({ category, handleValidate }) {
     )
 }
 
-export default AchatList
+export default CommandeList
